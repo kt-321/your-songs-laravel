0c7c2a0cd1b977857c48fbfb3aa4594a5a08cd5f
kt <kt.shzk247@gmail.com>
2019-06-22 06:16:54 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -6,12 +6,27 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         
-        <div class="collapse navbar-collapse" id="nav-bar">
+        <div class="collapse navbar-collapse" id="nav_bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-sign-in-alt mr-1"></i>ログイン</a></li>
-                <li class="nav-item"><a href="{{ route("signup.get") }}" class="nav-link"><i class="fas fa-user-plus mr-1"></i>新規登録</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                <!--ログイン中のナビゲーションバー-->
+                @if (Auth::check())
+                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right bg-dark border-white">
+                            <li class="dropdown-item"><a href="#" class="text-white"><i class="fas fa-user-circle mr-1" style="color: white;"></i>マイページ</a></li>
+                            <li class="dropdown-divider"></li>
+                            <li class="dropdown-item"><a href="{{ route("logout.get") }}" class="text-white"><i class="fas fa-sign-out-alt mr-1" style="color: white;"></i>ログアウト</a></li>
+                        </ul>
+                    </li>
+                    
+                <!--ログアウト中のナビゲーションバー-->
+                @else
+                    <li class="nav-item"><a href="{{ route("login") }}" class="nav-link"><i class="fas fa-sign-in-alt mr-1"></i>ログイン</a></li>
+                    <li class="nav-item"><a href="{{ route("signup.get") }}" class="nav-link"><i class="fas fa-user-plus mr-1"></i>新規登録</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the YourSongs</h1>
-            <a href="#" class="btn btn-lg btn-primary"><i class="fas fa-sign-out-alt mr-1"></i>ログイン</a>
+            <a href="{{ route("login") }}" class="btn btn-lg btn-primary"><i class="fas fa-sign-out-alt mr-1"></i>ログイン</a>
             <a href="{{ route("signup.get") }}" class="btn btn-lg btn-success"><i class="fas fa-user-plus mr-1"></i>新規登録</a>
         </div>
     </div>
+    @endif
 @endsection
