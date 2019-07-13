4b432e76627e8ea8fd42f1ad3ac80c9287cf2d0d
kt <kt.shzk247@gmail.com>
2019-07-13 02:56:14 +0000

    create favorites-index and modify navtabs

--- resources/views/users/navtabs.blade.php
+++ resources/views/users/navtabs.blade.php
@@ -6,5 +6,5 @@
     @endif
     <li class="nav-item"><a href="{{ route("users.followings", ["id" => $user->id]) }}" class="nav-link {{ Request::is("users/*/followings") ? "active" : ""}}"><i class="fas fa-user"></i>フォロー中<span class="badge badge-secondary ml-1">{{ $count_followings }}</span></a></li>
     <li class="nav-item"><a href="{{ route("users.followers", ["id" => $user->id]) }}" class="nav-link {{ Request::is("users/*/followers") ? "active" : ""}}"><i class="fas fa-user"></i>フォロワー<span class="badge badge-secondary ml-1">{{ $count_followers }}</span></a></li>
-    <li class="nav-item"><a href="#"><i class="fas fa-star mr-2"></i>お気に入り</a></li>
+    <li class="nav-item"><a href="{{ route("users.favorites", ["id" => $user->id])}}" class="nav-link {{ Request::is("users/*/favorites") ? "active" : ""}}"><i class="fas fa-star"></i> お気に入り <span class="badge badge-secondary ml-1">{{ $count_favorites }}</span></a></li>
 </ul>
