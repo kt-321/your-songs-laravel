12ff579a491e6d8724a130cd166f9217b8a03142
kt <kt.shzk247@gmail.com>
2019-08-13 06:10:33 +0000

    install Vue.js and VueCarousel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,10 @@
         <meta charset="utf-8">
         <title>YourSongs</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{ asset('/css/app.css') }}" type="text/css" rel="stylesheet">
         <link href="{{ asset('/css/style.css') }}" type="text/css" rel="stylesheet">
+        <script src="{{ asset('/js/app.js') }}" defer></script>
     </head>
     
     <body>
@@ -21,9 +23,6 @@
             <small>&copy; 2019 YourSongs</small>
         </footer>
         
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
     </body>
 </html>
