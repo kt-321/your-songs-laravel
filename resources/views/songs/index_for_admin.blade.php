b2905fe2e2ccd27fce851af900304ed650038714
kt <kt.shzk247@gmail.com>
2019-08-25 02:27:30 +0000

    modify the table element

--- resources/views/songs/index_for_admin.blade.php
+++ resources/views/songs/index_for_admin.blade.php
@@ -9,7 +9,7 @@
 <div class="users-index mb-3">
     <h4 class="text-center">未削除</h4>
     <table border="1" class="m-auto text-center">
-        <tbody>
+        <thead>
             <tr>
                 <th>曲ID</th>
                 <th>曲名</th>
@@ -20,30 +20,33 @@
                 <th>曲の詳細</th>
                 <th>削除</th>
             </tr>
-            @forelse ($songs as $song)
-                <tr>
-                    <td>{{ $song->id }}</td>
-                    <td>{{ $song->title }}</td>
-                    <td>{{ $song->artist_name }}</td>
-                    <td>{{ $song->music_age }}</td>
-                    <td>{{ $song->user_id }}</td>
-                    <td><a href="{{ route("users.show", ["id" => $song->user->id]) }}">{{ $song->user->name }}</a></td>
-                    <td>
-                         <a class="btn btn-success btn-sm" href="{{ route("songs.show", ["id" => $song->id]) }}">曲の詳細</a>
-                    </td>
-                    <td>
-                        <a href="/delete/{{ $song->id }}" onclick="delete_alert();return false;">削除</a>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
-            @endforelse
+        </thead>
+        
+        <tbody>
+        @forelse ($songs as $song)
+            <tr>
+                <td>{{ $song->id }}</td>
+                <td>{{ $song->title }}</td>
+                <td>{{ $song->artist_name }}</td>
+                <td>{{ $song->music_age }}</td>
+                <td>{{ $song->user_id }}</td>
+                <td><a href="{{ route("users.show", ["id" => $song->user->id]) }}">{{ $song->user->name }}</a></td>
+                <td>
+                     <a class="btn btn-success btn-sm" href="{{ route("songs.show", ["id" => $song->id]) }}">曲の詳細</a>
+                </td>
+                <td>
+                    <a href="/delete/{{ $song->id }}" onclick="delete_alert();return false;">削除</a>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>
+        @endforelse
         </tbody>
     </table>
 </div>
@@ -51,7 +54,7 @@
 <div class="deleted-users-index">
     <h4 class="text-center">削除済</h4>
     <table border="1" class="m-auto text-center">
-        <tbody>
+        <thead>
             <tr>
                 <th>曲ID</th>
                 <th>曲名</th>
@@ -62,33 +65,36 @@
                 <th>復旧</th>
                 <th>完全削除</th>
             </tr>
-            @forelse ($deleted as $delete)
-                <tr>
-                    <td>{{ $delete->id }}</td>
-                    <td>{{ $delete->title }}</td>
-                    <td>{{ $delete->artist_name }}</td>
-                    <td>{{ $delete->music_age }}</td>
-                    <td>{{ $delete->user_id }}</td>
-                    <td><a href="{{ route("users.show", ["id" => $delete->user->id]) }}">{{ $delete->user->name }}</a></td>
-                    <td>
-                        <a href="/restore/{{ $delete->id }}">復旧</a>
-                    </td>
-                    <td>
-                        <a href="/force-delete/{{ $delete->id }}">完全削除</a>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
-            @endforelse
+        </thead>
+        
+        <tbody>
+        @forelse ($deleted as $delete)
+            <tr>
+                <td>{{ $delete->id }}</td>
+                <td>{{ $delete->title }}</td>
+                <td>{{ $delete->artist_name }}</td>
+                <td>{{ $delete->music_age }}</td>
+                <td>{{ $delete->user_id }}</td>
+                <td><a href="{{ route("users.show", ["id" => $delete->user->id]) }}">{{ $delete->user->name }}</a></td>
+                <td>
+                    <a href="/restore/{{ $delete->id }}">復旧</a>
+                </td>
+                <td>
+                    <a href="/force-delete/{{ $delete->id }}">完全削除</a>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>
+        @endforelse
         </tbody>
     </table>
 </div>
