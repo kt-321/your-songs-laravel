f7cb8cab31bbada4105c3a5585cc0adb1d315f98
kt <kt.shzk247@gmail.com>
2019-09-14 06:50:29 +0000

    set up for phpunit-test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    public function from(string $url)
+    {
+        session()->setPreviousUrl(url($url));
+        return $this;
+    }
 }
