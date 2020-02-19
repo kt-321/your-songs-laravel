e87c3395f72468b1c0badfce3f9deebafa9be95b
kt-321 <kt.shzk247@gmail.com>
2020-02-19 23:27:16 +0900

    不要なルーティングを削除

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,7 @@ use Laravel\Passport\Passport;
 |
 */
 
+// ログインしているユーザーの情報を返す
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});;
-
-Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('/user/{id}', 'UsersController@user');
 });
