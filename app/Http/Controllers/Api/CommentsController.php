669756b4a537e1c97e3fa6c7f181f6f3d67a31de
kt-321 <kt.shzk247@gmail.com>
2020-03-15 20:02:18 +0900

    不要なコメントを削除

--- app/Http/Controllers/Api/CommentsController.php
+++ app/Http/Controllers/Api/CommentsController.php
@@ -13,29 +13,22 @@ use App\Http\Controllers\Controller;
 use Log;
 
 class CommentsController extends Controller
-{      
-    // public function store(CreateCommentRequest $request, $id)
+{
     public function store(Request $request, $sid)
     {
         $user = \Auth::user();
         $song = Song::find($sid);
-        // Log::debug($song);
-        // $request->merge(['user_id' => $user->id, 'song_id' => $song->id]);
         $request->merge(['user_id' => $user->id, 'song_id' => $song->id]);
-        // $song->comments()->create($request->validated());
         $song->comments()->create($request->all());
     }
     public function update(Request $request, $sid, $cid)
     {
         $user = \Auth::user();
         $song = Song::find($sid);
-        // Log::debug($song);
-        // $request->merge(['user_id' => $user->id, 'song_id' => $song->id]);
         $comment = Comment::find($cid);
         Log::debug($comment);
         $request->merge(['user_id' => $user->id, 'song_id' => $song->id]);
         Log::debug($request);
-        // $song->comments()->create($request->validated());
         $comment->update($request->all());
     }
  
